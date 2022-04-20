634c41479dd3bef3312068b5b63f6c5415a5e29e
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2022-04-20 10:23:30 +0300

    server

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+const mysql = require('mysql');
+
+
+
+var app = express();
+
+// var pool = mysql.createPool({
+//     host:'34.116.183.91',
+//     socketPath:'/cloudsql/medicineapi:europe-central2:medicinedbinstance', // comment this when running locally
+//     database: 'questions',
+//     user:'root',
+//     password:'1234',
+// }) 
+
+app.get("/", (req, res, next) => {
+
+    res.json("test")
+   
+});
+
+
+
+app.listen(800, () => {
+
+ console.log("Server running on port 800");
+
+});
+
